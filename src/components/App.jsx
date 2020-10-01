f4a9450e57f90f954b8684ba80fb901f99a841e4
Vikram S <vikram.senthil@outlook.com>
2020-10-01 20:27:28 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-
+import CreateArea from "./CreateArea";
 function App() {
+  const [notesArr, addNotesArr] = useState([])
+  function handleClick(notes) {
+    addNotesArr(prevValue => {
+      return [...prevValue,
+        notes
+      ]
+    })
+  }
+
+  // Two Ways of implementing
+  // function deleteNotes(id) {
+  //   addNotesArr(notesArr.filter((notes, index) => {
+  //     return index !== id;
+  //   }));
+  // }
+
+  function deleteNotes(id) {
+    addNotesArr((prevValue)=> prevValue.filter((notes, index) => {
+      return index !== id;
+    }));
+  }
+
+
+
+
   return (
     <div>
       <Header />
-      <Note />
+      <CreateArea onClicked={handleClick} />
+      {notesArr.map((notes, index) => <Note key={index} id={index} title={notes.title} content={notes.content} onClicked={deleteNotes} />)}
+
       <Footer />
     </div>
   );
 }
-
 export default App;
