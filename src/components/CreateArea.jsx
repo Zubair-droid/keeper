2268befa29744361bcc5680062999e0d0d843cfb
Vikram S <vikram.senthil@outlook.com>
2020-10-01 21:45:41 +0530

    added function for CreateArea component

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -2,3 +2,24 @@ import React,{useState} from "react";
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
 import Zoom from '@material-ui/core/Zoom';
+function CreateArea(props) {
+    return (
+        <div>
+       <form autocomplete="off" className= "create-note">
+               {bool&&<input name="title" placeholder="Title"  value={notes.title} onChange={handleChange}/>}
+               <textarea name="content" placeholder="Take a note..." rows={bool===false?"1":"3"} value={notes.content} onChange={handleChange} onClick={handleClick} />
+              <Zoom in = {!bool?false:true}><Fab onClick= {(event)=>
+               {
+                   props.onClicked(notes);
+                   setNotes({
+                       title:"",
+                       content:"" 
+                   }
+                  );
+                  setBool(false);
+       event.preventDefault();
+       }}><AddIcon/></Fab></Zoom> 
+             </form>
+           </div>
+         );
+       }
